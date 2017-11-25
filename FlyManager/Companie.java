bb58ee6a36bb0bb531a5f5d731314ccc45ef945b
yassern1 <yassern1@gmail.com>
2017-11-25 11:06:56 +0000

    First Commit

---
+++ FlyManager/Companie.java
@@ -0,0 +1,104 @@
+/***********************************************************************
+ * Module:  Companie.java
+ * Author:  Toshiba
+ * Purpose: Defines the Class Companie
+ ***********************************************************************/
+
+import java.util.*;
+
+public class Companie {
+   /** @pdOid 3eded0d4-2bb9-46ac-8a9e-37dfe1d40f6d */
+   private String idCompanie;
+   /** @pdOid 93de42e8-ce85-4a6d-9256-dbd93a8cb76d */
+   private String nomCompanie;
+   
+   /** @pdRoleInfo migr=no name=Vol assc=proposer coll=java.util.Collection impl=java.util.HashSet mult=1..* */
+   public java.util.Collection<Vol> vol;
+   
+   /** @pdOid 2d8a6abe-645a-4e27-abef-10369c573309 */
+   public String getIdCompanie() {
+      return idCompanie;
+   }
+   
+   /** @param newIdCompanie
+    * @pdOid ecef0e0f-20b1-4f3c-b8ae-46794b63fd94 */
+   public void setIdCompanie(String newIdCompanie) {
+      idCompanie = newIdCompanie;
+   }
+   
+   /** @pdOid 8ab1de12-263d-4e18-8b2d-b2a9031f4fef */
+   public String getNomCompanie() {
+      return nomCompanie;
+   }
+   
+   /** @param newNomCompanie
+    * @pdOid c5237c0f-9bb4-462b-9b20-52fbc7242cbc */
+   public void setNomCompanie(String newNomCompanie) {
+      nomCompanie = newNomCompanie;
+   }
+   
+   
+   /** @pdGenerated default getter */
+   public java.util.Collection<Vol> getVol() {
+      if (vol == null)
+         vol = new java.util.HashSet<Vol>();
+      return vol;
+   }
+   
+   /** @pdGenerated default iterator getter */
+   public java.util.Iterator getIteratorVol() {
+      if (vol == null)
+         vol = new java.util.HashSet<Vol>();
+      return vol.iterator();
+   }
+   
+   /** @pdGenerated default setter
+     * @param newVol */
+   public void setVol(java.util.Collection<Vol> newVol) {
+      removeAllVol();
+      for (java.util.Iterator iter = newVol.iterator(); iter.hasNext();)
+         addVol((Vol)iter.next());
+   }
+   
+   /** @pdGenerated default add
+     * @param newVol */
+   public void addVol(Vol newVol) {
+      if (newVol == null)
+         return;
+      if (this.vol == null)
+         this.vol = new java.util.HashSet<Vol>();
+      if (!this.vol.contains(newVol))
+      {
+         this.vol.add(newVol);
+         newVol.setCompanie(this);      
+      }
+   }
+   
+   /** @pdGenerated default remove
+     * @param oldVol */
+   public void removeVol(Vol oldVol) {
+      if (oldVol == null)
+         return;
+      if (this.vol != null)
+         if (this.vol.contains(oldVol))
+         {
+            this.vol.remove(oldVol);
+            oldVol.setCompanie((Companie)null);
+         }
+   }
+   
+   /** @pdGenerated default removeAll */
+   public void removeAllVol() {
+      if (vol != null)
+      {
+         Vol oldVol;
+         for (java.util.Iterator iter = getIteratorVol(); iter.hasNext();)
+         {
+            oldVol = (Vol)iter.next();
+            iter.remove();
+            oldVol.setCompanie((Companie)null);
+         }
+      }
+   }
+
+}
---
+++ FlyManager/Vol.java
@@ -0,0 +1,84 @@
+/***********************************************************************
+ * Module:  Vol.java
+ * Author:  Toshiba
+ * Purpose: Defines the Class Vol
+ ***********************************************************************/
+
+import java.util.*;
+
+
+public class Vol {
+   /** @pdOid 896afed9-c429-410e-8794-8c6e5ed53215 */
+   private String idVol;
+   /** @pdOid 89e14f60-76d6-4e4c-8a42-f16b5e721818 */
+   private String description;
+   /** @pdOid e68dbc03-99c6-4455-aaf4-a9e20d25c2e5 */
+   private Date dateVol;
+   
+   /** @pdRoleInfo migr=no name=Companie assc=proposer mult=1..1 side=A */
+   public Companie companie;
+   
+   /** @pdOid 48e71364-1887-4baf-af7a-a7b552b98097 */
+   public Vol() {
+      // TODO: implement
+   }
+   
+   /** @pdOid 28706dcd-0e86-43d2-baae-e430941c7498 */
+   public String getIdVol() {
+      return idVol;
+   }
+   
+   /** @param newIdVol
+    * @pdOid 4dd3ca1f-8b75-4eb2-8a3b-346694159142 */
+   public void setIdVol(String newIdVol) {
+      idVol = newIdVol;
+   }
+   
+   /** @pdOid cd09a5c3-e694-4a86-9b8a-5e24c724d591 */
+   public String getDescription() {
+      return description;
+   }
+   
+   /** @param newDescription
+    * @pdOid 5b38b462-cce3-4bb7-8635-651029e1440d */
+   public void setDescription(String newDescription) {
+      description = newDescription;
+   }
+   
+   /** @pdOid dc1914d0-83a8-401b-8022-e97bfa3e123e */
+   public Date getDateVol() {
+      return dateVol;
+   }
+   
+   /** @param newDateVol
+    * @pdOid 6617391c-63c5-435f-a106-dda4e0a8d376 */
+   public void setDateVol(Date newDateVol) {
+      dateVol = newDateVol;
+   }
+   
+   
+   /** @pdGenerated default parent getter */
+   public Companie getCompanie() {
+      return companie;
+   }
+   
+   /** @pdGenerated default parent setter
+     * @param newCompanie */
+   public void setCompanie(Companie newCompanie) {
+      if (this.companie == null || !this.companie.equals(newCompanie))
+      {
+         if (this.companie != null)
+         {
+            Companie oldCompanie = this.companie;
+            this.companie = null;
+            oldCompanie.removeVol(this);
+         }
+         if (newCompanie != null)
+         {
+            this.companie = newCompanie;
+            this.companie.addVol(this);
+         }
+      }
+   }
+
+}
